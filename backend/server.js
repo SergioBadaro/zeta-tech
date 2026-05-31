5a5fb479c448a660399664e4c77720ba76074f4e
SergioBadaro <sergiobadaro@hotmail.com>
2026-05-31 01:58:15 -0300

    Implementação do Backend no sistema utilizando Node.js e express

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor rodando na porta ${process.env.PORT}`);
+});
